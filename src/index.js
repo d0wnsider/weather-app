6f2ca8654066c0f983530a10f031ffc133dc7b14
d0wnsider <jmhandsamson@gmail.com>
2023-08-27 22:43:32 -0400

    Additional updates

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const locTime = document.querySelector('.locTime');
 const tempC = document.querySelector('.tempC');
 const tempF = document.querySelector('.tempF');
 const locCondition = document.querySelector('.locCondition');
+const locDay = document.querySelector('.locDay');
+const humidity = document.querySelector('.humidity');
 
 fetch(
   'https://api.weatherapi.com/v1/current.json?key=346db19fc2d3410b90233359231708&q=new york&aqi=no',
@@ -16,10 +18,14 @@ fetch(
     return response.json();
   })
   .then(function (data) {
+    console.log(data);
     loc.textContent = data.location.name;
+    if (data.location.is_day === 0) locDay.textContent = 'Day';
+    else locDay.textContent = 'Night';
     locTime.textContent = data.location.localtime;
     tempC.textContent = data.current.temp_c;
     tempF.textContent = data.current.temp_f;
     locCondition.textContent = data.current.condition.text;
+    humidity.textContent = data.current.humidity;
   })
   .catch((error) => console.error('Error:', error));
