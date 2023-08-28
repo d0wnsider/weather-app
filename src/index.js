07a14dbe9fa1effe5c7a0e27b248ec376b431065
d0wnsider <jmhandsamson@gmail.com>
2023-08-27 22:27:07 -0400

    Removed unnecessary data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 /* eslint-disable prefer-arrow-callback */
 /* eslint-disable func-names */
 const loc = document.querySelector('.loc');
-const img = document.querySelector('img');
+const locTime = document.querySelector('.locTime');
+const tempC = document.querySelector('.tempC');
+const tempF = document.querySelector('.tempF');
+const locCondition = document.querySelector('.locCondition');
 
 fetch(
   'https://api.weatherapi.com/v1/current.json?key=346db19fc2d3410b90233359231708&q=new york&aqi=no',
@@ -13,19 +16,10 @@ fetch(
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
-    console.log(data.location.name);
     loc.textContent = data.location.name;
-    console.log(data.location.localtime);
-    console.log(data.current.temp_c);
-    console.log(data.current.temp_f);
-    console.log(data.current.condition.text);
-    console.log(data.current.condition.icon);
-    img.src = data.current.condition.icon;
-    console.log(data.current.wind_mph);
-    console.log(data.current.wind_dir);
-    console.log(data.current.humidity);
-    console.log(data.current.feelslike_c);
-    console.log(data.current.feelslike_f);
+    locTime.textContent = data.location.localtime;
+    tempC.textContent = data.current.temp_c;
+    tempF.textContent = data.current.temp_f;
+    locCondition.textContent = data.current.condition.text;
   })
   .catch((error) => console.error('Error:', error));
